b11bee7406378528fbf89b7c59d61591ab3cc550
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-24 08:48:49 +0530

    style developer-responsive

--- src/components/Developer/Developer.jsx
+++ src/components/Developer/Developer.jsx
@@ -10,9 +10,8 @@ const Developer = () => {
                 <div className="left">
                     <h2>Superpowers for DeFi developers.</h2>
                     <p>Checkout the
-                        <span className="blue">documentation </span>
-                        , the
-                        <span className="blue">quick start</span>
+                        <span className="blue"> documentation</span>,
+                        the <span className="blue">quick start </span>
                         or a guide below to integrate your project with
                         thousands of tokens and billions of liquidity.
                     </p>

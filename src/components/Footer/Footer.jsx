fc6c9e1192da679e0638de8a0da7e5234df8d506
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-25 06:16:55 +0530

    build footer-section

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="container">
+                <div className="col">
+                    <h4>Platform</h4>
+                    <p>Home</p>
+                    <p>Blog</p>
+                    <p>Careers</p>
+                    <p>Legal & Privacy</p>
+                </div>
+                <div className="col">
+                    <h4>Developers</h4>
+                    <p>API</p>
+                    <p>Swap</p>
+                    <p>Token</p>
+                    <p>Staking</p>
+                </div>
+                <div className="col">
+                    <h4>Governance</h4>
+                    <p>App</p>
+                    <p>Defi</p>
+                    <p>Token</p>
+                    <p>Analytics</p>
+                </div>
+                <div className="col">
+                    <h4>Community</h4>
+                    <p>Events</p>
+                    <p>Protocol</p>
+                    <p>Workshops</p>
+                    <p>Hackathons</p>
+                </div>
+                <div className="col">
+                    <h4>About</h4>
+                    <p>Team</p>
+                    <p>Founders</p>
+                    <p>Economics</p>
+                    <p>Investors</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
